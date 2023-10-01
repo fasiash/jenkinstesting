8c3a51b06a3bfa9164f1db5ff7e5a3c08a811d39
fasiash <fasiuddin@codetru.com>
2023-10-01 10:52:06 +0530

    pom

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,10 +1,10 @@
 package com.qascript;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class FirstTest {
 
@@ -19,7 +19,7 @@ public class FirstTest {
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+        Assert.assertEquals("Page title is not correct",driver.getTitle().equals("Google"));
     }
 
 
